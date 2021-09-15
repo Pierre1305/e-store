ff8a1c737d6324d3fd9ade651319d08082412df4
alexstenberg <70631861+alexstenberg@users.noreply.github.com>
2021-09-15 12:39:09 +0200

    update

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ async function GetApiData()
 
 // Admin
 
+// SIDE NAVIGATION
+function openNav() {
+    document.getElementById('mySidenav').style.width = '175px';
+    document.getElementById('main').style.marginLeft = '250px';
+  }
+  
+  function closeNav() {
+    document.getElementById('mySidenav').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+  }
+
