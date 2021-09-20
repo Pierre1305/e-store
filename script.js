e191065ad595595dac824242454a9ea35939c1d3
alexstenberg <70631861+alexstenberg@users.noreply.github.com>
2021-09-20 15:39:06 +0200

    update

--- script.js
+++ script.js
@@ -1,11 +1,83 @@
-// Hämta data från API
+// ______________________________________________________________//
+// API / JSON
+// ______________________________________________________________//
 
-async function GetApiData() {
-}
+/* async function getShopData(apiUrl) {
+    axios.get(apiUrl)
+        .then(response => {
+            app.posts = response.data;
+            console.log("Hämtar återigen!")
+        })
+} */
 
-// Admin
+const app = new Vue({
+    el: '#app',
+    async created() { 
+        await axios.get('https://fakestoreapi.com/products')
+        .then(res => { app.products = res.data; console.log(res.data); });
+    },
+    data: {
+        products: [],
+        cart: [],
+    },
+    methods: {
+        createProduct: function () {
+            this.products.push({ 
+                // title: this.prodName, price: '4.99' 
+            })
+        },       
+    }
+});
 
+const categoryView = Vue.component('category', {
+        data: function () {
+            return {
+                category: category
+            }
+        },
+    template: ''
+});
+
+const appComponent = Vue.component('info', {
+    data: function () {
+        return {
+            theShop: app.posts
+        }
+    },
+    template: '<div><ol>'
+        + '<li v-for="product in theShop">{{product.title}} ${{product.price}}</li></ol>'
+        + '</div>'
+});
+/* async function GetApiData() {
+    await fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(json => console.log(json));
+} */
+
+
+// ______________________________________________________________//
+// PRODUCTS
+// ______________________________________________________________//
+
+// ______________________________________________________________//
+// PRODUCTS DETAILED
+// ______________________________________________________________//
+
+// ______________________________________________________________//
+// ADMIN
+// ______________________________________________________________//
+
+// ______________________________________________________________//
+// CHECKOUT
+// ______________________________________________________________//
+
+// ______________________________________________________________//
+// PAYMENT & SHIPPING
+// ______________________________________________________________//
+
+// ______________________________________________________________//
 // SIDE NAVIGATION
+// ______________________________________________________________//
 
 function openNav() {
     document.getElementById('mySidenav').style.width = '175px';
